609c7009b629aacbfd4dbdc7016b02ae065375ea
GUSTAVO ANTONIOLI MEDEIROS <94537799+GustaAntonioli@users.noreply.github.com>
2023-02-05 23:03:36 -0300

    ✓

--- OrdemInversa.java
+++ OrdemInversa.java
@@ -12,14 +12,14 @@ public class OrdemInversa {
 
         System.out.print("Vetor: ");
         int count =0;
-        while(count < (vetor.length)) {
-            System.out.print(vetor[count] + " ");
-            count++;
+        while(count < (vetor.length)) {                     //Nesse trecho do código foi utilizado o while,
+            System.out.print(vetor[count] + " ");   //-->   //mas poderia ser o for também.Irá mostrar o vetor em
+            count++;                                        //ordem crescente.
         }
     
-        System.out.print("\nVetor: ");
-        for(int i = (vetor.length - 1); i >= 0; i --) {
-            System.out.print(vetor[i] + " ");
+        System.out.print("\nVetor: ");              //-->   //Nesse trecho do código foi utilizado o for,            
+        for(int i = (vetor.length - 1); i >= 0; i --) {     //mas poderia ser o while também.Irá mostrar o vetor em
+            System.out.print(vetor[i] + " ");               //ordem decrescente.
         }
     }
 }
