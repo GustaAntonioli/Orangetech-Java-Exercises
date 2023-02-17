e86ed15ab2eb5864106c0257c128abe1843cd955
GUSTAVO ANTONIOLI MEDEIROS <94537799+GustaAntonioli@users.noreply.github.com>
2023-02-16 21:45:07 -0300

    ✓

--- nomeIdade2.java
+++ nomeIdade2.java
@@ -5,9 +5,8 @@ public class nomeIdade2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        String nome = "";
-        int idade = 0; //Para erro da variável (notUsed), inseri 
-                       //a condição if (idade == 0) break; no último bloco de código.
+        String nome;
+        int idade;
 
         while(true) {
             System.out.println("Nome: ");
@@ -16,12 +15,9 @@ public class nomeIdade2 {
 
             System.out.println("idade: ");
             idade = scan.nextInt();
-            if (idade == 0) break;
-
         }
-        scan.close(); // Para o erro do scanNeverClose
+        
         System.out.println("continue aqui...");
     }
-
 }
 
